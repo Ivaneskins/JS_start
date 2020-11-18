b11c3222018816c9e8fcfbcd4ec7f83ad3da6ecd
Ivan <example@gmail.com>
2020-11-18 16:00:27 +0500

    first console app

--- JS/script.js
+++ JS/script.js
@@ -8,10 +8,27 @@
 // const answer = +prompt('Вам есть 18?', '18');
 // console.log(typeof(answer));
 
-const answers = [];
+// const answers = [];
 
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваша фамиля?', '');
-answers[2] = prompt('Сколько вам лет?', '');
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамиля?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
 
-document.write(answers);
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
