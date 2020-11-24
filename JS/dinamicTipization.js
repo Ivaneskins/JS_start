3902d76d79ecbab60245b3f248357e7599fe20ca
Ivan <example@gmail.com>
2020-11-24 08:38:30 +0500

    get_elem_DOM

--- JS/dinamicTipization.js
+++ JS/dinamicTipization.js
@@ -1,1 +1,47 @@
 "use strict";
+
+// 1)
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+//2)
+console.log(typeof(5 + '')); //string!
+
+const num = 5;
+
+console.log('https://vk.com/catalog/' + num);
+
+const fontSize = 26 + 'px';
+
+// to number
+
+// 1)
+console.log(typeof(Number('4')));
+
+// 2)
+console.log(typeof(+'5'));
+
+// 3)
+console.log(typeof(parseInt('15px', 10)));
+
+let answ = +prompt('hello', '');
+
+// To boolean
+
+// 0, '', null, undefined, NaN;
+
+
+// 1)
+let switcher = null;
+
+if (switcher) {
+    console.log();
+}
+
+// 2)
+console.log(typeof(Boolean('4')));
+
+// 3)
+console.log(typeof(!!'4444'));
+
+
